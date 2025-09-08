10b868a5806aca952461f5cebe9ddf0d984d0573
Gennadiy Saranchuk <gennadiy.saranchuk@gmail.com>
2025-09-08 18:20:26 +0500

    feat: add class AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { State } from './reducers';
+import { loadForms, resetForm } from './components/form/actions/form.actions';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Tour of Heroes';
+
+  constructor(private store: Store<State>) {}
+
+  ngOnInit() {
+    // this.store.dispatch(loadForms());
+  }
+
+  openForm() {
+    this.store.dispatch(loadForms());
+  }
+
+  resetForm() {
+    this.store.dispatch(resetForm());
+  }
 }
