f96e4e4eee02ca6b1ac6bf57ba00f9988d8e1010
Gennadiy Saranchuk <gennadiy.saranchuk@gmail.com>
2025-09-08 18:28:30 +0500

    feat: add reducers and effects to AppModule imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
+import { NgModule, isDevMode } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService } from './in-memory-data.service';
-
 import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { HeroesComponent } from './heroes/heroes.component';
 import { HeroSearchComponent } from './hero-search/hero-search.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { InMemoryDataService } from './in-memory-data.service';
 import { MessagesComponent } from './messages/messages.component';
+import { MyCounterComponent } from './components/my-counter/my-counter.component';
+import { metaReducers, reducers } from './reducers';
+import { AppEffects } from './app.effects';
+import { AppFormModule } from './components/form/form.module';
+import { HeroEffects } from './heroes/store/heroes.effects';
 
 @NgModule({
   imports: [
@@ -21,9 +26,14 @@ import { MessagesComponent } from './messages/messages.component';
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
-
+    AppFormModule,
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
       dataEncapsulation: false,
+    }),
+    EffectsModule.forRoot([AppEffects, HeroEffects]),
+    StoreModule.forRoot(reducers, { metaReducers }),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
     }),
   ],
   declarations: [
@@ -33,6 +43,7 @@ import { MessagesComponent } from './messages/messages.component';
     HeroDetailComponent,
     MessagesComponent,
     HeroSearchComponent,
+    MyCounterComponent,
   ],
   bootstrap: [AppComponent],
 })
